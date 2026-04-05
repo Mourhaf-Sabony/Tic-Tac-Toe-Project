462ed2d5b5668cffca92acfb7c09acf395a56f8f
mourhaf-sabony <mourhafsabonys@gmail.com>
2026-04-05 15:12:34 +0300

    Fix form size and update logic in Form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -79,7 +79,7 @@
             this.pb3.TabIndex = 10;
             this.pb3.TabStop = false;
             this.pb3.Tag = "?";
-            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            this.pb3.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb2
             // 
@@ -92,7 +92,7 @@
             this.pb2.TabIndex = 9;
             this.pb2.TabStop = false;
             this.pb2.Tag = "?";
-            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            this.pb2.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb1
             // 
@@ -105,7 +105,7 @@
             this.pb1.TabIndex = 8;
             this.pb1.TabStop = false;
             this.pb1.Tag = "?";
-            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            this.pb1.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb5
             // 
@@ -118,7 +118,7 @@
             this.pb5.TabIndex = 7;
             this.pb5.TabStop = false;
             this.pb5.Tag = "?";
-            this.pb5.Click += new System.EventHandler(this.pb5_Click);
+            this.pb5.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb4
             // 
@@ -131,7 +131,7 @@
             this.pb4.TabIndex = 6;
             this.pb4.TabStop = false;
             this.pb4.Tag = "?";
-            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            this.pb4.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb9
             // 
@@ -144,7 +144,7 @@
             this.pb9.TabIndex = 5;
             this.pb9.TabStop = false;
             this.pb9.Tag = "?";
-            this.pb9.Click += new System.EventHandler(this.pb9_Click);
+            this.pb9.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb8
             // 
@@ -157,7 +157,7 @@
             this.pb8.TabIndex = 4;
             this.pb8.TabStop = false;
             this.pb8.Tag = "?";
-            this.pb8.Click += new System.EventHandler(this.pb8_Click);
+            this.pb8.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb7
             // 
@@ -170,7 +170,7 @@
             this.pb7.TabIndex = 3;
             this.pb7.TabStop = false;
             this.pb7.Tag = "?";
-            this.pb7.Click += new System.EventHandler(this.pb7_Click);
+            this.pb7.Click += new System.EventHandler(this.pb_Click);
             // 
             // pb6
             // 
@@ -183,7 +183,7 @@
             this.pb6.TabIndex = 2;
             this.pb6.TabStop = false;
             this.pb6.Tag = "?";
-            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            this.pb6.Click += new System.EventHandler(this.pb_Click);
             // 
             // pictureBox10
             // 
@@ -284,7 +284,9 @@
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic-Tac-Toe Game";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
